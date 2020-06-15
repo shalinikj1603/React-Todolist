578d4ad9eb9df59c019899f32aeb13a97c99a6bc
shalinikj <shalini.jagadish@geneia.com>
2020-06-15 15:03:58 +0530

    Updates

---
+++ precache-manifest.415f016cf2e28d3137b19f178603f04f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48e4285027626649884b0058297d24c",
+    "url": "/React-Todolist/index.html"
+  },
+  {
+    "revision": "655669efa5fd56909773",
+    "url": "/React-Todolist/static/css/main.4b15a28e.chunk.css"
+  },
+  {
+    "revision": "e154e0b240ea87cd475d",
+    "url": "/React-Todolist/static/js/2.0a725467.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todolist/static/js/2.0a725467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655669efa5fd56909773",
+    "url": "/React-Todolist/static/js/main.7a214206.chunk.js"
+  },
+  {
+    "revision": "ff3d1d1c00e655c41fe7",
+    "url": "/React-Todolist/static/js/runtime-main.36acdc14.js"
+  }
+]);
